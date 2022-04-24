c6d3ed58a85d7bac482e5356b11b0c8a74999dde
Admin <aliaskarguur1980@gmail.com>
2022-04-24 14:24:11 +0600

    Initial commit

---
+++ src/main/java/com/aliaskar/EcommerceSpringBoot/config/Webconfig.java
@@ -0,0 +1,24 @@
+package com.aliaskar.EcommerceSpringBoot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/**
+ * Created by Admin on 24.04.2022
+ */@Configuration
+class Webconfig  {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
